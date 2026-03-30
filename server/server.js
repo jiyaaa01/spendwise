73b3e7e167012b2972a5f2ab5ee9249e9a5521ee
jiya chauhan <jiyachauhan711@gmail.com>
2026-03-31 00:25:36 +0530

    added production build

--- server/server.js
+++ server/server.js
@@ -31,3 +31,11 @@ mongoose.connect(process.env.MONGO_URI)
     );
   })
   .catch(err => console.error('MongoDB connection error:', err));
+
+  const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
