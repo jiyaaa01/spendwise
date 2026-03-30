8679b178748ec014e634aa193eef4ae3f9adde39
jiya chauhan <jiyachauhan711@gmail.com>
2026-03-31 00:48:24 +0530

    fixed build error and ready for deploy

--- client/src/index.js
+++ client/src/index.js
@@ -25,10 +25,3 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
